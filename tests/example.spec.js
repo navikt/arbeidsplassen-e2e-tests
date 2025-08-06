98ab2a005aa143ec913c69a06bc756237328f274
Audun Skjelvan <audun.skjelvan@nav.no>
2025-08-06 15:04:25 +0200

    add WIP favorite test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -61,3 +61,36 @@ test("/stillinger is working and count is above 0", async ({ page }) => {
 
   expect(number).toBeGreaterThan(0);
 });
+
+test("Favorites are working in DEV", async ({ page }) => {
+  await page.goto("https://arbeidsplassen.intern.dev.nav.no/");
+
+  const login = page.locator("button", { hasText: "Logg inn" }).first();
+  await login.click();
+
+  expect(page.url()).toMatch(
+    `https://login.test.idporten.no/authorize/selector`
+  );
+
+  const loginIdPorten = page.locator("a", { hasText: "TestID" }).first();
+  await loginIdPorten.click();
+
+  expect(
+    page.url().startsWith("https://testid.test.idporten.no/authorize")
+  ).toBe(true);
+
+  await page.fill("input[id=pid]", "07499738492");
+
+  await page.click("button[type=submit]");
+  console.log("URL", page.url());
+
+  expect(page.url()).toMatch(`https://arbeidsplassen.intern.dev.nav.no/`);
+
+  await expect(page).toHaveTitle(
+    "Arbeidsplassen.no - Alle ledige jobber, samlet på én plass"
+  );
+
+  await expect(
+    page.locator("a", { hasText: "Min side" }).first()
+  ).toBeVisible();
+});
