9fa395c3a7e3863bcde48e36e0b163d8ffc4a2d2
Audun Skjelvan <audun.skjelvan@nav.no>
2025-09-09 13:01:36 +0200

    test prod link check

--- tests/prod-checklinks.spec.js
+++ tests/prod-checklinks.spec.js
@@ -25,6 +25,9 @@ async function validateLink(link, page) {
         },
         timeout: 20000, // 20 second timeout
       }); // equivalent to command "curl -I <link>"
+      if (response) {
+        console.log("R", response);
+      }
 
       if (response?.status() === 429) {
         console.warn(
@@ -33,6 +36,7 @@ async function validateLink(link, page) {
         // eslint-disable-next-line no-await-in-loop
         await randomDelay(1000, 10000); // Vent 1 sekund og prøv igjen
       } else if (response?.status() < 400 || response?.status() === 401) {
+        console.log(`VALID ${link}`);
         return null;
       } else {
         console.error(
@@ -48,126 +52,130 @@ async function validateLink(link, page) {
 
 test("Check links on pages", async ({ page }) => {
   test.setTimeout(10 * 60 * 1000); // 10 minute timeout
-  const baseUrl = getProdDomain();
+  // const baseUrl = getProdDomain();
+  const baseUrl =
+    "https://uustatus.no/nb/erklaringer/publisert/9f7beaf9-ea64-4a93-8e20-8282f8fd1fce";
 
   const visitedUrls = new Set();
   const urlsToVisit = new Set([baseUrl]);
   const linkIssues = {};
   const maxPagesToCheck = 500; // Limit to 500 pages
 
-  const additionalPages = ["/bedrift"];
-  additionalPages.forEach((pagePath) => {
-    const fullUrl = new URL(pagePath, baseUrl).toString();
-    urlsToVisit.add(fullUrl);
-  });
+  await validateLink(baseUrl, page);
 
-  const checkLinks = async (url) => {
-    if (visitedUrls.has(url) || visitedUrls.size >= maxPagesToCheck) return;
+  // const additionalPages = ["/bedrift"];
+  // additionalPages.forEach((pagePath) => {
+  //   const fullUrl = new URL(pagePath, baseUrl).toString();
+  //   urlsToVisit.add(fullUrl);
+  // });
 
-    const isSameDomain = new URL(url).hostname === new URL(baseUrl).hostname;
-    if (isSameDomain && visitedUrls.size < maxPagesToCheck) {
-      console.log(`Checking: ${url}`);
-      await page.waitForTimeout(1500);
-      await page.goto(url, { waitUntil: "domcontentloaded" });
-      await page.waitForLoadState("networkidle");
-      visitedUrls.add(url);
+  // const checkLinks = async (url) => {
+  //   if (visitedUrls.has(url) || visitedUrls.size >= maxPagesToCheck) return;
 
-      // Always validate the current URL
-      const validationError = await validateLink(url, page);
-      if (validationError) {
-        console.error(`Broken link found on: ${page.url()} ${url}`);
+  //   const isSameDomain = new URL(url).hostname === new URL(baseUrl).hostname;
+  //   if (isSameDomain && visitedUrls.size < maxPagesToCheck) {
+  //     console.log(`Checking: ${url}`);
+  //     await page.waitForTimeout(1500);
+  //     await page.goto(url, { waitUntil: "domcontentloaded" });
+  //     await page.waitForLoadState("networkidle");
+  //     visitedUrls.add(url);
 
-        linkIssues[url] = `Broken link found on ${url} -> ${page.url()}`;
-      }
+  //     // Always validate the current URL
+  //     const validationError = await validateLink(url, page);
+  //     if (validationError) {
+  //       console.error(`Broken link found on: ${page.url()} ${url}`);
 
-      // Only collect more links if we haven't reached the limit and this is our domain
-      const links = await page.$$eval(
-        "a[href]",
-        (anchors, base) =>
-          anchors
-            .map((anchor) => anchor.href)
-            .filter((href) => {
-              // Skip if href is empty
-              if (!href) return false;
+  //       linkIssues[url] = `Broken link found on ${url} -> ${page.url()}`;
+  //     }
 
-              // Skip specific paths
-              const excludedPaths = [
-                "/stillinger/stilling",
-                "/stillingsregistrering",
-                "/cv",
-                "/oauth2",
-                "https://login.idporten.no/",
-              ];
+  //     // Only collect more links if we haven't reached the limit and this is our domain
+  //     const links = await page.$$eval(
+  //       "a[href]",
+  //       (anchors, base) =>
+  //         anchors
+  //           .map((anchor) => anchor.href)
+  //           .filter((href) => {
+  //             // Skip if href is empty
+  //             if (!href) return false;
 
-              if (excludedPaths.some((path) => href.includes(path))) {
-                return false;
-              }
+  //             // Skip specific paths
+  //             const excludedPaths = [
+  //               "/stillinger/stilling",
+  //               "/stillingsregistrering",
+  //               "/cv",
+  //               "/oauth2",
+  //               "https://login.idporten.no/",
+  //             ];
 
-              // Skip anchors, mailto, and tel links
-              if (
-                href.includes("#") ||
-                href.includes("mailto:") ||
-                href.includes("tel:")
-              ) {
-                return false;
-              }
+  //             if (excludedPaths.some((path) => href.includes(path))) {
+  //               return false;
+  //             }
 
-              return true;
-            }),
-        baseUrl
-      );
+  //             // Skip anchors, mailto, and tel links
+  //             if (
+  //               href.includes("#") ||
+  //               href.includes("mailto:") ||
+  //               href.includes("tel:")
+  //             ) {
+  //               return false;
+  //             }
 
-      for (const link of links) {
-        if (visitedUrls.size >= maxPagesToCheck) break;
-        const linkUrl = new URL(link);
-        // Only add to visit queue if it's the same domain
-        if (linkUrl.hostname === new URL(baseUrl).hostname) {
-          if (!visitedUrls.has(link) && !urlsToVisit.has(link)) {
-            urlsToVisit.add(link);
-          }
-        } else {
-          // Validate external links but don't follow them
-          try {
-            const error = await validateLink(link, page);
-            if (error) {
-              console.error(`Broken external link found on ${url}: ${link}`);
-              linkIssues[
-                url
-              ] = `Broken external link found on ${url} -> ${link}`;
-            } else {
-              console.log(`VALID: ${link}`);
-            }
-          } catch (err) {
-            console.error(`Error validating external link ${link}:`, err);
-          }
-        }
-      }
-    } else {
-      // Validate external links but don't follow them
-      try {
-        const error = await validateLink(url, page);
-        if (error) {
-          console.error(`Broken external link found on ${url}: ${url}`);
-          linkIssues[url] = `Broken external link found on ${url} -> ${url}`;
-        } else {
-          console.log(`VALID: ${url}`);
-        }
-      } catch (err) {
-        console.error(`Error validating external link ${url}:`, err);
-      }
-    }
-  };
+  //             return true;
+  //           }),
+  //       baseUrl
+  //     );
 
-  while (urlsToVisit.size > 0 && visitedUrls.size < maxPagesToCheck) {
-    const [nextUrl] = urlsToVisit;
-    await checkLinks(nextUrl);
-    urlsToVisit.delete(nextUrl);
-  }
+  //     for (const link of links) {
+  //       if (visitedUrls.size >= maxPagesToCheck) break;
+  //       const linkUrl = new URL(link);
+  //       // Only add to visit queue if it's the same domain
+  //       if (linkUrl.hostname === new URL(baseUrl).hostname) {
+  //         if (!visitedUrls.has(link) && !urlsToVisit.has(link)) {
+  //           urlsToVisit.add(link);
+  //         }
+  //       } else {
+  //         // Validate external links but don't follow them
+  //         try {
+  //           const error = await validateLink(link, page);
+  //           if (error) {
+  //             console.error(`Broken external link found on ${url}: ${link}`);
+  //             linkIssues[
+  //               url
+  //             ] = `Broken external link found on ${url} -> ${link}`;
+  //           } else {
+  //             console.log(`VALID: ${link}`);
+  //           }
+  //         } catch (err) {
+  //           console.error(`Error validating external link ${link}:`, err);
+  //         }
+  //       }
+  //     }
+  //   } else {
+  //     // Validate external links but don't follow them
+  //     try {
+  //       const error = await validateLink(url, page);
+  //       if (error) {
+  //         console.error(`Broken external link found on ${url}: ${url}`);
+  //         linkIssues[url] = `Broken external link found on ${url} -> ${url}`;
+  //       } else {
+  //         console.log(`VALID: ${url}`);
+  //       }
+  //     } catch (err) {
+  //       console.error(`Error validating external link ${url}:`, err);
+  //     }
+  //   }
+  // };
 
-  // Log all link issues found
-  const issueCount = Object.keys(linkIssues).length;
-  if (issueCount > 0) {
-    const errorMessages = Object.values(linkIssues).join("\n");
-    throw new Error(`Found ${issueCount} broken links:\n\n${errorMessages}`);
-  }
+  // while (urlsToVisit.size > 0 && visitedUrls.size < maxPagesToCheck) {
+  //   const [nextUrl] = urlsToVisit;
+  //   await checkLinks(nextUrl);
+  //   urlsToVisit.delete(nextUrl);
+  // }
+
+  // // Log all link issues found
+  // const issueCount = Object.keys(linkIssues).length;
+  // if (issueCount > 0) {
+  //   const errorMessages = Object.values(linkIssues).join("\n");
+  //   throw new Error(`Found ${issueCount} broken links:\n\n${errorMessages}`);
+  // }
 });
